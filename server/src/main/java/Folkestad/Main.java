cb4f3dc606e7f0a22c1addec9e44856e30dfe08a
Oddvar Folkestad <oddvarhf@ntnu.no>
2025-07-06 13:12:48 +0200

    feat(main) la til springboot main

--- server/src/main/java/Folkestad/Main.java
+++ server/src/main/java/Folkestad/Main.java
@@ -1,7 +1,28 @@
 package Folkestad;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * The main class for the Server Application.
+ * This class is responsible for bootstrapping the Spring Boot application.
+ */
+@SpringBootApplication(scanBasePackages = {"Folkestad"})
+@ComponentScan(basePackages = {"Folkestad.Project", "Folkestad.server", "Folkestad.dto", "Folkestad.model"})
+@EntityScan("Folkestad.model")
+@EnableJpaRepositories("Folkestad.model")
+@EnableScheduling // Aktiverer planlegging av oppgaver
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    /**
+     * The main method which serves as the entry point for the Spring Boot application.
+     *
+     * @param args command-line arguments passed to the application
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(Main.class, args);
     }
 }
