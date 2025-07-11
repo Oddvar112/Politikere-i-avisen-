cbfca76271976172b2cb42f091a6ed1ae558a542
Oddvar Folkestad <oddvarhf@ntnu.no>
2025-07-11 17:31:58 +0200

    refactor(Person): remove manual getters and setters, add Lombok annotations

--- model/src/main/java/Folkestad/Person.java
+++ model/src/main/java/Folkestad/Person.java
@@ -1,44 +1,32 @@
 package Folkestad;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "links") // Avoid circular reference in toString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     private String name;
 
     @OneToMany(mappedBy = "person", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<PersonLink> links = new HashSet<>();
-
-    // Getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<PersonLink> getLinks() {
-        return links;
-    }
-
-    public void setLinks(Set<PersonLink> links) {
-        this.links = links;
-    }
 }
