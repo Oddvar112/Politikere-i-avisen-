1f4488b5351cef4d5b5a4f91bb8e5f8a8e25f6da
Oddvar Folkestad <oddvarhf@ntnu.no>
2025-07-11 17:31:50 +0200

    feat(model): add KandidatStortingsvalg entity with JPA annotations

---
+++ model/src/main/java/Folkestad/KandidatStortingsvalg.java
@@ -0,0 +1,65 @@
+package Folkestad;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+import lombok.Builder;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "kandidat_stortingsvalg")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class KandidatStortingsvalg {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "valg")
+    private String valg;
+
+    @Column(name = "valgdistrikt")
+    private String valgdistrikt;
+
+    @Column(name = "partikode")
+    private String partikode;
+
+    @Column(name = "partinavn")
+    private String partinavn;
+
+    @Column(name = "display_order")
+    private Integer displayOrder;
+
+    @Column(name = "kandidatnr")
+    private Integer kandidatnr;
+
+    @Column(name = "navn")
+    private String navn;
+
+    @Column(name = "bosted")
+    private String bosted;
+
+    @Column(name = "stilling")
+    private String stilling;
+
+    @Column(name = "foedselsdato")
+    private LocalDate foedselsdato;
+
+    @Column(name = "alder")
+    private Integer alder;
+
+    @Column(name = "kjoenn")
+    private String kjoenn;
+}
