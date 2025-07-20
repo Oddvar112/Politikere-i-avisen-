f29131e59c1a0299948180f7faeba47d795dfefc
Oddvar Folkestad <hanev.hanevik@gmail.com>
2025-07-20 16:02:57 +0200

    feat(repository): Add method to find Innlegg by normalized URL

--- model/src/main/java/Folkestad/InnleggRepository.java
+++ model/src/main/java/Folkestad/InnleggRepository.java
@@ -1,6 +1,8 @@
 package folkestad;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
@@ -8,8 +10,12 @@ import java.util.Optional;
 @Repository
 public interface InnleggRepository extends JpaRepository<Innlegg, Long> {
     Optional<Innlegg> findByLink(String link);
+    
+    @Query("SELECT i FROM Innlegg i WHERE i.link = :baseUrl OR i.link LIKE CONCAT(:baseUrl, '?%')")
+    Optional<Innlegg> findByNormalizedUrl(@Param("baseUrl") String baseUrl);
+    
     boolean existsByLink(String link);
     List<Innlegg> findBySammendragIsNotNull();
     long countBySammendragIsNotNull();
-    
+
 }
