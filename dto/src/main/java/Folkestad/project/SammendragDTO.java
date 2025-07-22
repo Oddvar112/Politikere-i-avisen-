ac134444b75343ca843b8eddd130faecca2a8501
Oddvar Folkestad <hanev.hanevik@gmail.com>
2025-07-22 18:36:51 +0200

    refactor(dto): Remove unused 'id' field and add 'opprettetDato' to SammendragDTO

--- dto/src/main/java/Folkestad/project/SammendragDTO.java
+++ dto/src/main/java/Folkestad/project/SammendragDTO.java
@@ -3,15 +3,16 @@ package folkestad.project;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SammendragDTO {
-    private Long id;
-    private String link;
+    private String link; 
     private String sammendrag;
     private Double kompresjonRatio;
     private Integer antallOrdOriginal;
     private Integer antallOrdSammendrag;
+    private LocalDateTime opprettetDato;
 }
